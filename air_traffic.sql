bba72c5461f7780d7933c04ab8c5cc060b13a24f
Fiona <chunmei.fiona.li@gmail.com>
2022-07-25 09:02:32 -0500

    first commit

---
+++ air_traffic.sql
@@ -0,0 +1,68 @@
+-- from the terminal run:
+-- psql < air_traffic.sql
+
+DROP DATABASE IF EXISTS air_traffic;
+
+CREATE DATABASE air_traffic;
+
+\c air_traffic
+
+CREATE TABLE passengers(
+  id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL
+);
+CREATE TABLE flights(
+  id SERIAL PRIMARY KEY,
+  departure TIMESTAMP NOT NULL,
+  arrival TIMESTAMP NOT NULL,
+  airline TEXT NOT NULL,
+  from_city TEXT NOT NULL,
+  from_country TEXT NOT NULL,
+  to_city TEXT NOT NULL,
+  to_country TEXT NOT NULL
+);
+CREATE TABLE tickets(
+  id SERIAL PRIMARY KEY,
+  seat TEXT NOT NULL,
+  passengers_id INTEGER REFERENCES passengers,
+  flight_id INTEGER REFERENCES flights
+);
+INSERT INTO passengers
+  (first_name, last_name)
+VALUES
+  ('Jennifer', 'Finch'),
+  ('Thadeus', 'Gathercoal'),
+  ('Sonja', 'Pauley'),
+  ('Waneta', 'Skeleton'),
+  ('Berkie', 'Wycliff'),
+  ('Alvin', 'Leathes'),
+  ('Cory', 'Squibbes');
+
+INSERT INTO flights
+  (departure, arrival, airline, from_city, from_country, to_city, to_country)
+VALUES
+  ('2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United', 'Washington DC', 'United States', 'Seattle', 'United States'),
+  ('2018-12-19 12:45:00', '2018-12-19 16:15:00', 'British Airways', 'Tokyo', 'Japan', 'London', 'United Kingdom'),
+  ('2018-01-02 07:00:00', '2018-01-02 08:03:00', 'Delta', 'Los Angeles', 'United States', 'Las Vegas', 'United States'),
+  ('2018-04-15 16:50:00', '2018-04-15 21:00:00', 'Delta', 'Seattle', 'United States', 'Mexico City', 'Mexico'),
+  ('2018-08-01 18:30:00', '2018-08-01 21:50:00', 'TUI Fly Belgium', 'Paris', 'France', 'Casablanca', 'Morocco'),
+  ('2018-10-31 01:15:00', '2018-10-31 12:55:00', 'Air China', 'Dubai', 'UAE', 'Beijing', 'China'),
+  ('2019-02-06 06:00:00', '2019-02-06 07:47:00', 'United', 'New York', 'United States', 'Charlotte', 'United States'),
+  ('2018-12-22 14:42:00', '2018-12-22 15:56:00', 'American Airlines', 'Cedar Rapids', 'United States', 'Chicago', 'United States'),
+  ('2019-02-06 16:28:00', '2019-02-06 19:18:00', 'American Airlines', 'Charlotte', 'United States', 'New Orleans', 'United States'),
+  ('2019-01-20 19:30:00', '2019-01-20 22:45:00', 'Avianca Brasil', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile');
+
+INSERT INTO tickets
+  (seat, passengers_id, flight_id)
+VALUES
+  ('33B',1,1),
+  ('8A',2,2),
+  ('12F',3,3),
+  ('20A',1,4),
+  ('23D',4,5),
+  ('18C',2,6),
+  ('9E',5,7),
+  ('1A',6,8),
+  ('32B',5,9),
+  ('10D',7,10);
